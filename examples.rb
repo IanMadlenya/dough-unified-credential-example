3d5bf028d738cbf6f9d70541cf332a367ae9d4fa
Dain Hall <dainovu3@gmail.com>
2014-09-17 10:59:12 -0500

    Added styling to examples

--- examples.rb
+++ examples.rb
@@ -1,6 +1,15 @@
 load File.expand_path('../config/environment.rb',  __FILE__)
 # Note: If you get a Faraday::ConnectionRefused ~> start DUC server or point in correct location
 
+def separator(test)
+  puts "\n========= #{test} ===========\n\n"
+end
+
+def results(response)
+  puts "\n--- results ---\n\n"
+  puts "status: #{response.status}"
+  puts "body: #{response.body}"
+end
 ## == Set port of DUC server (only setup for local)=============== ##
 
 client = DoughUnifiedCredential::Client.new(port: 3000)
@@ -11,7 +20,11 @@ client.routes
 
 ## == Routes also function as methods with an optional argument == ##
 
-client.new_session
+separator("new session")
+response = client.new_session
+puts "\n--- results ---\n\n"
+puts "status: #{response.status}"
+# should be 406 - new session not set up for json requests
 
 ## == Create user ================================================ ##
 
@@ -23,7 +36,8 @@ new_user = {
             :password_confirmation => "password"
            }
 
-response = client.create_registration(new_user)
+separator "create user"
+results client.create_registration(new_user)
 
 # -- response types ---------------------------- ##
 # response.status
@@ -38,13 +52,12 @@ existing_user = {
                   email: "dain@email.com",
                   password: "password"
                 }
-response = client.create_session(new_user)
+
+separator("create session")
+results client.create_session(existing_user)
 
 # -- response types ---------------------------- ##
 # response.status
 # response.body
 # 404
 # 201 - Success - resource as json object
-
-## == Delete session ============================================= ##
-
--- lib/client.rb
+++ lib/client.rb
@@ -19,7 +19,7 @@ module DoughUnifiedCredential
     ROUTES.each do |controller, actions|
       actions.each do |action, detail|
         define_method "#{action}_#{controller.gsub(/(s*)$/, '')}" do |*args|
-          perform(detail["method"].downcase.to_sym, detail["path"].concat('.json'), build_request_format(args.first || {}))
+          perform(detail["method"].downcase.to_sym, detail["path"].concat(".json"), build_request_format(args.first || {}))
         end
       end
     end
@@ -31,8 +31,8 @@ module DoughUnifiedCredential
     private
 
     def build_request_format(payload = {})
-      # 1. add user node to user parameters
-      params = {user: payload.permit(:email, :name, :nickname, :password, :password_confirmation, :current_password)}
+      # 1. make sure user node before user parameters
+      params = if payload.empty? then {} else {user: payload.permit(:email, :name, :nickname, :password, :password_confirmation, :current_password)} end
       # 2. add client domain and hashify signature
       signatured_params = params.merge(client_name: ENV["DOMAIN_NAME"])
       request_signature = HmacUtils.sign_message(signatured_params)
@@ -43,8 +43,8 @@ module DoughUnifiedCredential
 
     def build_client(options)
       @client = Faraday.new(:url => "http://localhost:#{options[:port]}") do |faraday|
-        faraday.request  :url_encoded             # form-encode POST params
-        faraday.response :logger                  # log requests to STDOUT
+        faraday.request  :url_encoded
+        faraday.response :logger
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
     end
